43e6b36b4949d822f6811603db2262ad86dc63aa
venagin <ilya02111999@mail.ru>
2022-03-28 23:18:32 +0300

    makefile update, vscode tasks, cpp statememts

--- makefile
+++ makefile
@@ -1,15 +1,22 @@
-name=123 $(CXX)
-sep=\\
-exec:=bin$(sep)unit.exe
-sep=/
-srcs=$(wildcard src/*.cpp)
-all: print build start 
-build:
-	$(CXX) $(srcs) -o $(exec) -std=c++17
-start:$(exec)
-	$(exec)
+NAME:=venagin_script
+COMPILER:=g++
+LINKER:=g++
+SRC:=src
+OBJ:=bin
+SEP:=/
+EXEC:=bin/unit.exe
+SRCS:=$(wildcard $(SRC)/*.cpp)
+OBJS:=$(patsubst $(SRC)/%.cpp,$(OBJ)/%.obj,$(SRCS))
+CFLAGS:=-std=c++17
+all: build start 
+build:$(OBJS)
+	$(LINKER) $(OBJS) -o $(EXEC)
+start:build
+	$(EXEC)
 print:
-	$(info $(name))
-	echo $(exec)
-clean:
-	del $(exec)
+	$(info $(SRCS))
+	$(info $(OBJS))
+$(OBJ)/%.obj: $(SRC)/%.cpp
+	$(COMPILER) -c $< -o $@ $(CFLAGS) 
+# g++ -c <путь до main.cpp>(в нашем примере) "Вместо $< подставляется $(SRC)/%.cpp (т.е. то что справа)"
+# -O <путь до соответсвующего объектного файла.obj> "Вместо $@ подставяется $(OBJ)/%.obj (т.е. то что слева)"
--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,13 @@ void test_operators();
 void test_filesystem();
 void test_pointers();
 void test_arrays();
+void test_statements();
 int main()
 {
-    ::test_operators();
-    ::test_filesystem();
-    ::test_pointers();
+    ::test_statements();
+    //::test_operators();
+    //::test_filesystem();
+    //::test_pointers();
     return 0;
 }
 void test_operators()
@@ -20,6 +22,8 @@ void test_operators()
     std::cout<<(void*)test_pointers<<std::endl;
     std::cout<<(void*)test_filesystem<<std::endl;
     std::cout<<(void*)main<<std::endl;
+    const wchar_t*wstr=L"Привет Мир!";
+    std::wcout<<*wstr<<std::endl;
 }
 void test_filesystem()
 {
@@ -31,7 +35,10 @@ void test_filesystem()
 }
 void test_pointers() 
 {
-    int mino=78;
+    int mino=78; 
+    ::std::cout<<"--mino==78="<<(--mino==78)<<'\n';
+    mino++;
+    ::std::cout<<"mino--==78="<<(mino--==78)<<'\n';
     mino=13654;
     int* ptr=&mino;
     ::std::cout<<&mino<<std::endl;
@@ -45,4 +52,53 @@ void test_arrays()
     int* ptr=arr;
     arr[632]=54;
     ptr[702]=389;
+}
+void test_statements()
+{
+    if (false)
+    {
+        std::cout<<"This is inside of if(true)"<<std::endl;
+    }
+    else if (true)
+    {
+        std::cout<<"This is inside of else if(false)"<<std::endl;
+    }
+    else 
+    {
+        std::cout<<"This is inside of else"<<std::endl;
+    }
+    bool flag=true;
+    while (flag) //while (eng)- пока "в то время как" 
+    {
+        flag=false;
+        ::std::cout<<"While"<<std::endl;
+        //break;
+    }
+    do 
+    {
+        flag=false;
+        ::std::cout<<"Do while"<<std::endl;
+    } while (flag);
+    // Первый часть - объявление переменных; Вторая часть - условие; Третья часть - что делать после каждой итерации(выполнение кода в скобочках "{}");
+    for (int i = 0; ++i <= 10;)
+    {
+        ::std::cout<<i<<std::endl;
+    }
+    int exp=1;
+    switch (exp)
+    {
+    case 0: //case(eng) -случай
+        ::std::cout<<"case zero"<<std::endl;
+        ::std::cout<<"case zero"<<std::endl;
+        break; //break - закончить (обязательно в случае использования case для того чтобы закончить исполнение)
+    
+    default:
+        break;
+
+    case 1:
+        ::std::cout<<"case one"<<std::endl;
+        ::std::cout<<"case one"<<std::endl;
+        break; //break - закончить (обязательно в случае использования case для того чтобы закончить исполнение)
+    }
+    
 }
